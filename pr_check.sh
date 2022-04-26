c415864932492eaec72bee6b470a22a33ad90b64
Derek Horton <dehort@redhat.com>
2022-04-25 22:42:24 -0500

    Try to fix a build issue

--- pr_check.sh
+++ pr_check.sh
@@ -13,3 +13,11 @@ CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
+
+# This code is to create a 'dummy' result file so Jenkins will not fail when smoke tests are disabled
+mkdir -p $ARTIFACTS_DIR
+cat << EOF > $ARTIFACTS_DIR/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
